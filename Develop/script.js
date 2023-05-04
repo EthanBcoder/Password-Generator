89ec273cc2da985f865d7e69160f0d94c6bed518
EthanBcoder <bolton.ethan13@gmail.com>
2023-05-03 18:20:17 -0700

    Password generator

--- Develop/script.js
+++ Develop/script.js
@@ -13,5 +13,45 @@ function writePassword() {
 
 }
 
+function getPasswordOptions() {
+  var length = parseInt(
+    prompt('How many characters would you like your password to contain?'),
+    10
+  );
+
+  if (Number.isNaN(length)) {
+    alert('Password length must be provided as a number');
+    return null;
+  }
+
+  var hasSpecialCharacters = confirm(
+    'Click OK to confirm including special characters.'
+  );
+
+  var hasNumericCharacters = confirm(
+    'Click OK to confirm including numeric characters.'
+  );
+
+
+  var hasLowerCasedCharacters = confirm(
+    'Click OK to confirm including lowercase characters.'
+  );
+
+  var hasUpperCasedCharacters = confirm(
+    'Click OK to confirm including uppercase characters.'
+  );
+}
+
+if (
+  hasSpecialCharacters === false &&
+  hasNumericCharacters === false &&
+  hasLowerCasedCharacters === false &&
+  hasUpperCasedCharacters === false
+) {
+  alert('Must select at least one character type');
+  return null;
+}
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
